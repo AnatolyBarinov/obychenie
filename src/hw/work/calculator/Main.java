cb00493afb454544e8f1ca8f7b400e12c8657c0c
Anatoly Barinov <barinov.t@mail.ru>
2023-08-15 13:14:33 +0300

    First

---
+++ src/hw/work/calculator/Main.java
@@ -0,0 +1,12 @@
+package hw.work.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        // При делении на 0 выдаст ошибку
+        int c = calc.divide.apply(a, b);
+        calc.println.accept(c);
+    }
+}
