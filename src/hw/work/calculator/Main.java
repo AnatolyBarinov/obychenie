8ec573532a02b880af1c670babc2abf6c0abb433
Anatoly Barinov <barinov.t@mail.ru>
2023-08-16 13:57:18 +0300

    s

--- src/hw/work/calculator/Main.java
+++ src/hw/work/calculator/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        // При делении на 0 Делить на 0 нельзяs
+        // При делении на 0 дилить на 0 нельзяasdasdasdasdasdasdasdasdasd
         int c = calc.divide.apply(a, b);
         calc.println.accept(c);
     }
