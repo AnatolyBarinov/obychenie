67c74977e830f86db1ad3f794683fb50be85ba9d
AnatolyBarinov <62007944+AnatolyBarinov@users.noreply.github.com>
2023-11-20 14:06:22 +0300

    Add files via upload

---
+++ src/ServletsContainers/src/main/java/org/example/repository/PostRepository.java
@@ -0,0 +1,37 @@
+package org.example.repository;
+
+import org.example.model.Post;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PostRepository {
+  private AtomicLong idCounter = new AtomicLong(0);
+  private Map<Long, Post> posts = new ConcurrentHashMap<>();
+
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(posts.get(id));
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0) {
+      long id = idCounter.incrementAndGet();
+      post.setId(id);
+    }
+    posts.put(post.getId(), post);
+    return post;
+  }
+
+  public void removeById(long id) {
+    posts.remove(id);
+  }
+}
